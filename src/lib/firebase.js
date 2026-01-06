d44c0bd8aff18ec91f164c61de4e079971a2ee44
ToeunVao <vaotoeun@gmail.com>
2026-01-05 23:06:24 -0500

    Fix merge conflicts

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,20 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyAGZBJFVi_o1HeGDmjcSsmCcWxWOkuLc_4",
+    authDomain: "nailexpress-10f2f.firebaseapp.com",
+    projectId: "nailexpress-10f2f",
+    storageBucket: "nailexpress-10f2f.appspot.com",
+    messagingSenderId: "1015991996673",
+    appId: "1:1015991996673:web:b6e8888abae83906d34b00"
 };
 
-// Initialize Firebase (Prevents duplicate app errors during development)
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Next.js specific: prevents initializing Firebase twice during hot reloads
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
